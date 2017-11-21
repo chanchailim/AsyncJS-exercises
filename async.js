1c3d9997ce39dc4d109cdc534719cda8cc81a067
Chanchai <chanchailim@gmail.com>
2017-11-21 11:05:56 +0700

    ReadFile with Sync and Async

--- async.js
+++ async.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
 
-const readFromFile = (fileName)=>{
-  // WRITE CODE HERE
+const readFromFile = (fileName) => {
+    // WRITE CODE HERE
+    return fs.readFileSync(fileName, 'utf-8');
 };
 
+const readFromFileAsync = (fileName, cb) => {
+    // WRITE CODE HERE
+
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+        if (!err) {
+            cb(data);
+        } else {
+            cb('error');
+        }
+    });
+}
+
 console.log('START');
-const data = readFromFile('sample.txt');
-console.log('END')
+readFromFileAsync('sample.txt', (data) => {
+    console.log(data)
+    console.log('END')
+});
